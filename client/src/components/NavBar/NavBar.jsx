4bc7e0530b675e63b29aff4aaa2a49e87adaeee0
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-10 04:45:03 +0100

    update (UI) create navigation bar

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import styles from './NavBar.module.css'
+
+function NavBar() {
+    const pages = ['Home', 'Software Projects', '3D Artwork', 'About', 'Skills', 'Contact']
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>Logo</div>
+
+            <nav aria-label="Main Naviagtion" className={styles.nav}>
+                <ul className={styles.ul}> { pages.map(page => <li value={page} className={styles.li}> {page} </li> )}</ul>
+            </nav>
+            
+            <div className={styles.themeToggle}>Toggle</div>
+        </header>
+    )
+}
+
+export default NavBar

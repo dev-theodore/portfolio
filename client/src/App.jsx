9f7571cebcaf7fe456e8d9243e385c47fb0f6b6d
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-11 20:04:09 +0100

    feat(footer): add footer to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import NavBar from "./components/NavBar/NavBar"
+import LandingPage from "./pages/LandingPage"
+import Footer from "./components/Footer/Footer"
+import "./App.css"
+
 
 function App() {
   return (
-    <div className="app"></div>
+    <div className="app">
+      <NavBar />
+
+      <main id="main-content">
+        <LandingPage />
+      </main>
+      
+      <Footer />
+    </div>
   )
 }
 

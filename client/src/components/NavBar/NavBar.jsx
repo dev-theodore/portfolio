0d7113ca34ce39cf1b85e4b2ec41365b072c5c36
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-11 19:54:52 +0100

    fix(navBar): resolve alignment and spacing

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
+import { Moon, SunMedium } from 'lucide-react'
 import styles from './NavBar.module.css'
 
+
 function NavBar() {
-    const pages = ['Home', 'Software Projects', '3D Artwork', 'About', 'Skills', 'Contact']
+    const pages = ['Home', 'Software Projects', '3D Artwork', 'About', 'Skills', 'Contact'];
+    const [theme, setTheme] = useState('dark')
+    
+
+    function changeTheme() {
+        theme === 'dark' ? setTheme('light') : setTheme('dark')
+    }
 
     return (
         <header className={styles.header}>
@@ -9,11 +18,27 @@ function NavBar() {
 
             <nav aria-label="Main Naviagtion" className={styles.nav}>
                 <ul className={styles.ul}>
-                    {pages.map(page => <li value={page} className={styles.li}> {page} </li> )}
+
+                    {pages.map(page => 
+                        <li value={page} 
+                            key={page}
+                            className={styles.li}> 
+                            {page} 
+                        </li> 
+                    )}
+
                 </ul>
             </nav>
+
+            <div className={styles.divider}></div>
             
-            <div className={styles.themeToggle}>x</div>
+            <div className={styles.toggle} onClick={changeTheme}>
+                {
+                    theme === 'dark' ? 
+                    <SunMedium size={16} /> : 
+                    <Moon size={16}/>
+                }
+            </div>
         </header>
     )
 }

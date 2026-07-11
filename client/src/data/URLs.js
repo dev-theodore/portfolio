be47945f9be882f778adb200d04287e91625e2d4
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-11 19:36:18 +0100

    add packages and redirect links

---
+++ client/src/data/URLs.js
@@ -0,0 +1,22 @@
+
+const URL = {
+    github: 'https://github.com/dev-theodore',
+    artsation: 'https://artstation.com/bronkisart',
+    X: 'https://x.com/iam_theod0re',
+    bronkis_X: 'https://x.com/bronkisart',
+    instagram: 'https://instagram.com/iam_theod0re',
+    bronkis_instagram: 'https://instagram.com/bronkisart',
+    prodviz_instagram: 'https://instagram.com/bronkis.visuals',
+    threads: 'https://threads.com/iam_theod0re',
+    bronkis_threads: 'https://threads.com/bronkisart',
+    youtube: 'https://youtube.com/@bronkisart',
+    linkedin: 'https://linkedin.com/theodore',
+    gmail: 'mbangatheodoreesong@gmail',
+    gmail_bronkis: 'contact.bronkis@gmail.com',
+    pinterest: '',
+    linktree: '',
+    gumroad: '',
+    patreon: '',
+}
+
+export default URL

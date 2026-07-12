4fd15d5b46134ea25a303428cb96d34a93b0e62a
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-12 18:06:59 +0100

    feat(images): add dummy image url sources

---
+++ client/src/data/imageSrc.js
@@ -0,0 +1,18 @@
+import image1 from './images/wallpaperflare.com_wallpaper (25).jpg'
+import image2 from './images/wallpaperflare.com_wallpaper (40).jpg'
+import image3 from './images/wallpaperflare.com_wallpaper (9).jpg'  
+import image4 from './images/wallpaperflare.com_wallpaper.jpg'
+import image5 from './images/wallpaperflare.com_wallpaper (25).jpg'
+import image6 from './images/wallpaperflare.com_wallpaper (2).jpg'
+
+
+const imageSrc = [
+    image2,
+    image1,
+    image4,
+    image3,
+    image5,
+    image6,
+]
+
+export default imageSrc

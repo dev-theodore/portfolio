5dfe67b8faced0307e099e0d5c48ebf29c0e05c5
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-13 21:42:40 +0100

    fis(bug): solved display clipping issue on scroll

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -1,21 +1,27 @@
 import { descriptionTrimer, titleTrimer } from '../../utils/word-trim'
 import styles from './Card.module.css'
 import Tabs from '../Tabs/Tabs'
+import { useState } from 'react'
 
 function Card({ title, description, image, category, style}) {
 
+    const [hover, setHover] = useState(false)
+    const [display, noDisplay] = [{display: 'flex', zIndex: '1'}, {display: 'none', zIndex: '1'}]
     const cardTitle = titleTrimer(title)
     const cardDesc = descriptionTrimer(description)
 
     return (
-        <article className={styles.card}>
+        <article  className={styles.card} >
 
-            <div className={styles.image}>
+            <div 
+                className={styles.image}
+                onMouseEnter={() => setHover(true)} 
+                onMouseLeave={()=> setHover(false)}>
 
                 <img src={image} alt='artwork image' />
-                <div className={styles.gradient}></div>
+                <div className={styles.gradient} style={hover ? display : noDisplay}></div>
 
-                <div className={styles.tags}>
+                <div className={styles.tags} style={hover ? display : noDisplay}>
                     <Tabs category={category} />
                     <Tabs category={style} />
                 </div>

cdc2c7a94025d6e4eac214083d8079308f395bd6
TheoX11 <93312786+TheoX11@users.noreply.github.com>
2026-07-16 21:51:35 +0100

    feat(icons): add icons for tabs

---
+++ client/src/data/icons.js
@@ -0,0 +1,60 @@
+import { SiJavascript, SiGit, SiGithub, SiReact, SiHtml5, SiCss, SiPostman, 
+SiVuedotjs, SiTypescript, SiPython, SiNextdotjs, SiPostgresql, SiMongodb } from '@icons-pack/react-simple-icons'
+import { SiSass, SiTailwindcss, SiNodedotjs, SiExpress, SiGraphql, SiAxios,
+SiAnimedotjs, SiGsap, SiFramer, SiNestjs, SiFigma, SiWordpress, SiMysql} from '@icons-pack/react-simple-icons'
+import { SiBlender, SiAutodeskmaya, SiUnity, SiUnrealengine, SiGodotengine, 
+SiDavinciresolve, SiKrita} from '@icons-pack/react-simple-icons'
+
+
+const uiIcons = [
+
+]
+
+const techIcons = [
+    {id: 1, name: 'React', icon: SiReact, color: 'default'},
+    {id: 2, name: 'JavaScript', icon: SiJavascript, color: 'default'},
+    {id: 3, name: 'HTML', icon: SiHtml5, color: 'default'},
+    {id: 4, name: 'CSS', icon: SiCss, color: 'default'},
+    {id: 5, name: 'Postman', icon: SiPostman, color: 'default'},
+    {id: 6, name: 'Git', icon: SiGit, color: 'default'},
+    {id: 7, name: 'GitHub', icon: SiGithub, color: 'grey'},
+    {id: 8, name: 'Vue.js', icon: SiVuedotjs, color: 'default'},
+    {id: 9, name: 'TypeScript', icon: SiTypescript, color: 'default'},
+    {id: 10, name: 'Sass', icon: SiSass, color: 'default'},
+    {id: 11, name: 'Tailwind CSS', icon: SiTailwindcss, color: 'default'},
+    {id: 12, name: 'Node.js', icon: SiNodedotjs, color: 'default'},
+    {id: 13, name: 'Express', icon: SiExpress, color: '#5FA04E'},
+    {id: 14, name: 'REST API', icon: SiBlender, color: 'default'},
+    {id: 15, name: 'GraphQL', icon: SiGraphql, color: 'default'},
+    {id: 16, name: 'Axios', icon: SiAxios, color: 'default'},
+    {id: 17, name: 'Anime.js', icon: SiAnimedotjs, color: 'white'},
+    {id: 18, name: 'GSAP', icon: SiGsap, color: 'default'},
+    {id: 19, name: 'Framer', icon: SiFramer, color: 'default'},
+    {id: 20, name: 'Python', icon: SiPython, color: 'default'},
+    {id: 21, name: 'Next.js', icon: SiNextdotjs, color: 'white'},
+    {id: 22, name: 'NestJS', icon: SiNestjs, color: 'default'},
+    {id: 23, name: 'Figma', icon: SiFigma, color: 'default'},
+    {id: 24, name: 'PostgreSQL', icon: SiPostgresql, color: 'default'},
+    {id: 25, name: 'MongoDB', icon: SiMongodb, color: 'default'},
+    {id: 23, name: 'WordPress', icon: SiWordpress, color: 'default'},
+    {id: 23, name: 'MySQL', icon: SiMysql, color: 'default'},
+]
+
+const artIcons = [
+    {id: 1, name: 'Blender', icon: SiBlender, color: 'default'},
+    {id: 2, name: 'Maya', icon: SiAutodeskmaya, color: 'default'},
+    {id: 3, name: 'Unreal', icon: SiUnrealengine, color: 'default'},
+    {id: 4, name: 'Unity', icon: SiUnity, color: 'default'},
+    {id: 5, name: 'Godot', icon: SiGodotengine, color: 'default'},
+    {id: 6, name: 'DaVinci Resolve', icon: SiDavinciresolve, color: 'default'},
+    {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},
+    // {id: 7, name: 'Krita', icon: SiKrita, color: 'default'},  
+]
+
+export { uiIcons, techIcons, artIcons }
